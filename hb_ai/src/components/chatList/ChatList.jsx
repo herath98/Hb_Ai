ab8eab38343847d8005f2969530a21f5889aa9ff
harsha <harshaudayanga401@gmail.com>
2024-08-28 15:51:14 +0530

    changes

--- hb_ai/src/components/chatList/ChatList.jsx
+++ hb_ai/src/components/chatList/ChatList.jsx
@@ -13,22 +13,27 @@ const ChatList = () => {
 
   return (
     <div className="chatList">
-      <span className="title">DASHBOARD</span>
-      <Link to="/dashboard">Create a new Chat</Link>
-      <Link to="/">Explore Lama AI</Link>
-      <Link to="/">Contact</Link>
-      <hr />
-      <span className="title">RECENT CHATS</span>
-      <div className="grid grid-cols-1 overflow-scroll scroll-hidden">
-        {isPending
-          ? "Loading..."
-          : error
-          ? "Something went wrong!"
-          : data?.map((chat) => (
-              <Link to={`/dashboard/chats/${chat._id}`} key={chat._id}>
-                {chat.title}
-              </Link>
-            ))}
+      <div className="scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar scrollbar-thumb-slate-700 scrollbar-track-slate-700 h-auto overflow-y-scroll">
+        <div className="grid grid-cols-1" >
+          <span className="title">DASHBOARD</span>
+          <Link to="/dashboard">Create a new Chat</Link>
+          <Link to="/">Explore Lama AI</Link>
+          <Link to="/">Contact</Link>
+        </div>
+       
+
+        <span className="title">RECENT CHATS</span>
+        <div className="grid grid-cols-1 ">
+          {isPending
+            ? "Loading..."
+            : error
+              ? "Something went wrong!"
+              : data?.map((chat) => (
+                <Link to={`/dashboard/chats/${chat._id}`} key={chat._id}>
+                  {chat.title}
+                </Link>
+              ))}
+        </div>
       </div>
       <hr />
       <div className="upgrade">
@@ -38,7 +43,7 @@ const ChatList = () => {
           <span>Get unlimited access to all features</span>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
--- hb_ai/tailwind.config.js
+++ hb_ai/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite-react/tailwind";
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    ...flowbite.content(), // Spread operator to avoid any issues
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+    require('tailwind-scrollbar')({ nocompatible: true }),
+  ],
 };
